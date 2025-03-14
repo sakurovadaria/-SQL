be26b99176d89f6787da45edb1de45494f63310b
sakurovadaria <sakurovadaria@gmail.com>
2025-03-14 23:40:09 +0500

    First commit

---
+++ src/main/java/SQL/Introduction/IntroductionInSQL/IntroductionInSqlApplication.java
@@ -0,0 +1,13 @@
+package SQL.Introduction.IntroductionInSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionInSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionInSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SQL/Introduction/IntroductionInSQL/IntroductionInSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package SQL.Introduction.IntroductionInSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionInSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
